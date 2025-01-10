560702db7e10ea7c341e36b5733295316f2bb188
Saurabhkumar Sharma <saurabhk.sharma1410@gmail.com>
2025-01-10 21:22:34 +0530

    minor changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,9 +7,10 @@ const Footer = () => {
             <p>We are the best crypto trading app in India, we provide our guidance at a very cheap price</p>
         </div>
         <div className='flex flex-col items-center'>
-            <img className='rounded-full h-24' src="https://avatars.githubusercontent.com/u/160416508?s=400&u=27907b40860779ce107e78e82fb553e9ea3b22a6&v=4" alt="saurabhkumar" />
+            {/* <img className='rounded-full h-24' src="https://avatars.githubusercontent.com/u/160416508?s=400&u=27907b40860779ce107e78e82fb553e9ea3b22a6&v=4" alt="saurabhkumar" /> */}
+            <img className='rounded-full h-24' src="https://avatars.githubusercontent.com/u/135420472?v=4" alt="Shubham Jaiswal" />
             <p>Founder</p>
-            <p>S K Sharma</p>
+            <p>Shubham Jaiswal</p>
         </div>
     </div>
   )
